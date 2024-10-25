e461508efe38b5038c20d581ee5512d451c35206
amar0898 <amar.pinjore0898@gmail.com>
2024-10-24 21:24:18 -0400

    Created and configured separate spring boot Docker profiles for all Spring Boot microservices. Built Docker images and containers for each microservice using Docker Compose. Published all microservices to Docker, ensuring containerized deployment for the entire application.

--- jobMicroservices/src/main/java/com/amar/jobMicroservices/job/clients/ReviewClient.java
+++ jobMicroservices/src/main/java/com/amar/jobMicroservices/job/clients/ReviewClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "REVIEWMICROSERVICES")
+@FeignClient(name = "REVIEWMICROSERVICES", url = "${review-service.url}")
 public interface ReviewClient {
 
     @GetMapping("/reviews")
